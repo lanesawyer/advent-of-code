462595f95c14656017e1a5910a80343a2fb33a31
Lane Sawyer <github@lanesawyer.dev>
2020-12-01 12:50:22 -0800

    Day 1 solution

--- rust-2020/src/lib.rs
+++ rust-2020/src/lib.rs
@@ -1,14 +1,56 @@
-pub type Answer = String;
+use std::str::FromStr;
+
+pub type Answer = i32;
 
 pub trait Day {
-    fn run(day: &str) -> Answer;
+    fn part_1(day: &str) -> Option<Answer>;
+    fn part_2(day: &str) -> Option<Answer>;
 }
 pub struct Day1;
 
 impl Day for Day1 {
-    fn run(_input: &str) -> Answer {
-        String::from("day 1")
+    fn part_1(input: &str) -> Option<Answer> {
+        let numbers = parse_input::<i32>(input);
+
+        // Approach: Go through each number in the list
+        // and search all the other numbers in the list
+        // for something that adds up to 2020
+        for n in &numbers {
+            let to_find = 2020 - *n;
+            if numbers.contains(&to_find) {
+                let answer = n * to_find;
+                return Some(answer);
+            }
+        }
+        None
     }
+
+    fn part_2(input: &str) -> Option<Answer> {
+        let numbers = parse_input::<i32>(input);
+
+        // Approach: For each pair of numbers in the list
+        // search all the other numbers in the list
+        // for something that adds up to 2020
+        for first in &numbers {
+            for second in &numbers {
+                if first + second > 2020 {
+                    continue;
+                }
+
+                let to_find = 2020 - first - second;
+                if numbers.contains(&to_find) {
+                    return Some(to_find * first * second)
+                }
+            }
+        }
+        None
+    }
+}
+
+fn parse_input<T: FromStr>(input: &str) -> Vec<T> {
+    input.split_whitespace()
+        .filter_map(|number| number.parse::<T>().ok())
+        .collect::<Vec<T>>()
 }
 
 #[cfg(test)]
@@ -16,7 +58,24 @@ mod tests {
     use crate::{Day, Day1};
 
     #[test]
-    fn day1_works() {
-        assert_eq!(Day1::run("1234"), "day 1");
+    fn day1_part1_works() {
+        let test_input = r#"1721
+            979
+            366
+            299
+            675
+            1456"#;
+        assert_eq!(Day1::part_1(test_input), Some(514579));
+    }
+
+    #[test]
+    fn day1_part2_works() {
+        let test_input = r#"1721
+            979
+            366
+            299
+            675
+            1456"#;
+        assert_eq!(Day1::part_2(test_input), Some(241861950));
     }
 }
--- rust-2020/src/main.rs
+++ rust-2020/src/main.rs
@@ -5,11 +5,19 @@ mod lib;
 fn main() {
     println!("==== Advent of Code 2020 ====");
     let day1 = read_input(1);
-    println!("{}", Day1::run(&day1));
+    match Day1::part_1(&day1) {
+        Some(answer) => println!("  Part 1: {}", answer),
+        None => println!("  Part 1: Not found"),
+    }
+    
+    match Day1::part_2(&day1) {
+        Some(answer) => println!("  Part 2: {}", answer),
+        None => println!("  Part 2: Not found"),
+    }
 }
 
 fn read_input(day: u8) -> String {
-    println!("  Day {}", day);
+    println!("Day {}", day);
     std::fs::read_to_string(format!("./input/day{}.txt", day))
         .expect(&format!("Something went wrong reading the file for day {}", day))
 }
