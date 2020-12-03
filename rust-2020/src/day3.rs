9e34c9235ccdfd42c2dc9c071d071745fbd977d6
Lane Sawyer <github@lanesawyer.dev>
2020-12-03 15:59:29 -0800

    Slight change of approach to solve part 2

--- rust-2020/src/day3.rs
+++ rust-2020/src/day3.rs
@@ -8,7 +8,7 @@ pub struct Day3;
 impl Day for Day3 {
     fn part_1(input: &str) -> Option<Answer> {
         // Create the counting function with the correct right and down slope
-        let tree_finder = make_tree_finder(3, 1);
+        let tree_finder = make_tree_finder(3);
 
         // Read each row and clean up the data so entries can be processed
         // then use the tree finder and add up all trees
@@ -25,51 +25,41 @@ impl Day for Day3 {
     fn part_2(input: &str) -> Option<Answer> {
         // Create all the slopes we need to check
         let tree_finders = vec![
-            make_tree_finder(1, 1),
-            make_tree_finder(3, 1),
-            make_tree_finder(5, 1),
-            make_tree_finder(7, 1),
-            make_tree_finder(1, 2),
+            (1, make_tree_finder(1)),
+            (1, make_tree_finder(3)),
+            (1, make_tree_finder(5)),
+            (1, make_tree_finder(7)),
+            (2, make_tree_finder(1)),
         ];
 
-        // Loop over our slopes and for each one do the same process as step 1
+        // Loop over our slopes and for each one do the same process as step 1,
+        // but skip over the iterator by the amount we move down each time
         let trees_multiplied = tree_finders
             .iter()
-            .map(|count_trees| {
+            .map(|(down, count_trees)| {
                 input
                     .split('\n')
-                    .filter(|s| s.trim().len() != 0)
+                    .filter(|s| !s.is_empty())
+                    .step_by(*down)
                     .enumerate()
                     .filter_map(count_trees)
                     .sum::<u64>()
             })
-            .fold(1, |acc, trees| {
-                // From the printing here I think all my down_slope = 1 things work and = 2 doesn't
-                // Because 286 is the right answer for at least the second in the list, so we know the
-                // algorithm works to a degree, even if the unit test doesn't
-                println!("{}", trees);
-                acc * trees
-            });
+            .product();
 
         Some(trees_multiplied)
     }
 }
 
-fn make_tree_finder(right_slope: usize, down_slope: usize) -> impl Fn((usize, &str)) -> Option<u64> {
+fn make_tree_finder(right_slope: usize) -> impl Fn((usize, &str)) -> Option<u64> {
     move |(index, line)| -> Option<u64> {
-        // Only even attempt to add trees if we're on a line that
-        // is on the down slop, which happens when the index evenly divides
-        if index % down_slope == 0 {
-            // For the line, look for the next character based on
-            // the row we're on times the right slope to find the rightward movement
-            // but then we mod it by the line's length to get the wrapping behavior
-            // so the pattern "repeats"
-            match line.trim().chars().nth((index * right_slope) % line.len()) {
-                Some(TREE_CHAR) => Some(1),
-                _ => None,
-            }
-        } else {
-            None
+        // For the line, look for the next character based on
+        // the row we're on times the right slope to find the rightward movement
+        // but then we mod it by the line's length to get the wrapping behavior
+        // so the pattern "repeats"
+        match line.trim().chars().nth((index * right_slope) % line.len()) {
+            Some(TREE_CHAR) => Some(1),
+            _ => None,
         }
     }
 }
