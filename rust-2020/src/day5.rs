b9b9bbd6fb8d880867ac295f995d6d90eb1b35df
Lane Sawyer <github@lanesawyer.dev>
2020-12-05 17:35:07 -0800

    Day 5

--- rust-2020/src/day5.rs
+++ rust-2020/src/day5.rs
@@ -5,11 +5,67 @@ pub struct Day5;
 
 impl Day for Day5 {
     fn part_1(input: &str) -> Option<Answer> {
-        todo!()
+        // Future me, make some closure functions to reduce duplication
+        let max =  input.split_whitespace()
+            .map(|line| {
+                let (row, _) = &line[..7].chars()
+                    .fold((0, 127), |(start, end), next| {
+                        let halfway = (start + end) / 2;
+                        match next {
+                            'F' => (start, halfway),
+                            'B' => (halfway + 1, end),
+                            _ => { unreachable!( )}
+                        }
+                    });
+                let (column, _) = &line[7..].chars()
+                    .fold((0, 7), |(start, end), next| {
+                        let halfway = (start + end) / 2;
+                        match next {
+                            'L' => (start, halfway),
+                            'R' => (halfway + 1, end),
+                            _ => unreachable!(),
+                        }
+                    });
+
+                let ans: u64 = (row * 8) + column;
+                ans
+            })
+            .max().unwrap();
+        Some(max)
     }
 
     fn part_2(input: &str) -> Option<Answer> {
-        todo!()
+        let mut seats =  input.split_whitespace()
+            .map(|line| {
+                let (row, _) = &line[..7].chars()
+                    .fold((0, 127), |(start, end), next| {
+                        let halfway = (start + end) / 2;
+                        match next {
+                            'F' => (start, halfway),
+                            'B' => (halfway + 1, end),
+                            _ => { unreachable!( )}
+                        }
+                    });
+                let (column, _) = &line[7..].chars()
+                    .fold((0, 7), |(start, end), next| {
+                        let halfway = (start + end) / 2;
+                        match next {
+                            'L' => (start, halfway),
+                            'R' => (halfway + 1, end),
+                            _ => unreachable!(),
+                        }
+                    });
+
+                let ans: u64 = (row * 8) + column;
+                ans
+            }).collect::<Vec<u64>>();
+            seats.sort();
+
+            let my_seat = (seats[0]..=seats[seats.len() - 1] as u64)
+                .zip(seats)
+                .find(|(expected, seat)| expected != seat);
+            
+        Some(my_seat.unwrap().0)
     }
 }
 
@@ -20,23 +76,15 @@ mod tests {
 
     #[test]
     fn part1_works() {
-        let test_input = r#"1721
-            979
-            366
-            299
-            675
-            1456"#;
-        assert_eq!(Day5::part_1(test_input), Some(514579));
+        let test_input = "FBFBBFFRLR";
+        assert_eq!(Day5::part_1(test_input), Some(357));
     }
 
     #[test]
+    #[ignore]
     fn part2_works() {
-        let test_input = r#"1721
-            979
-            366
-            299
-            675
-            1456"#;
-        assert_eq!(Day5::part_2(test_input), Some(241861950));
+        // Not sure how to make this easily since it wasn't provided
+        let test_input = "";
+        assert_eq!(Day5::part_2(test_input), Some(0));
     }
 }
