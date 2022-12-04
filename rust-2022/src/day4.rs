c10ec3bdda15ae75909a649600a452dc7f191928
Lane Sawyer <github@lanesawyer.dev>
2022-12-03 21:53:56 -0800

    Solved day 4 (ugly)

---
+++ rust-2022/src/day4.rs
@@ -0,0 +1,100 @@
+use aoc_utils::{test_day, AdventError, Answer, Day};
+
+pub struct Day4;
+
+impl Day for Day4 {
+    fn part_1(input: &str) -> Result<Answer, AdventError> {
+        let fully_contained = input
+            .lines()
+            .filter_map(|line| {
+                let line = line.trim();
+                let mut elf_groups = line.split(',');
+                let first_elf = elf_groups.next().unwrap();
+                let second_elf = elf_groups.next().unwrap();
+
+                let first_elf: Vec<&str> = first_elf.split('-').collect();
+                let second_elf: Vec<&str> = second_elf.split('-').collect();
+
+                let first_elf_lower = first_elf[0].parse::<u64>().unwrap();
+                let first_elf_higher = first_elf[1].parse::<u64>().unwrap();
+
+                let second_elf_lower = second_elf[0].parse::<u64>().unwrap();
+                let second_elf_higher = second_elf[1].parse::<u64>().unwrap();
+
+                let is_fully_contained = (first_elf_lower >= second_elf_lower
+                    && first_elf_higher <= second_elf_higher)
+                    || (second_elf_lower >= first_elf_lower
+                        && second_elf_higher <= first_elf_higher);
+                if is_fully_contained {
+                    Some(1)
+                } else {
+                    None
+                }
+            })
+            .count();
+        Ok(fully_contained as u64)
+    }
+
+    fn part_2(input: &str) -> Result<Answer, AdventError> {
+        let fully_contained = input
+            .lines()
+            .filter_map(|line| {
+                let line = line.trim();
+                let mut elf_groups = line.split(',');
+                let first_elf = elf_groups.next().unwrap();
+                let second_elf = elf_groups.next().unwrap();
+
+                let first_elf: Vec<&str> = first_elf.split('-').collect();
+                let second_elf: Vec<&str> = second_elf.split('-').collect();
+
+                let first_elf_lower = first_elf[0].parse::<u64>().unwrap();
+                let first_elf_higher = first_elf[1].parse::<u64>().unwrap();
+
+                let second_elf_lower = second_elf[0].parse::<u64>().unwrap();
+                let second_elf_higher = second_elf[1].parse::<u64>().unwrap();
+
+                // There is horrific because there is absolutely a way to simplify this
+                // but I want to get the answer first
+                let is_fully_contained = (first_elf_lower >= second_elf_lower
+                    && first_elf_higher <= second_elf_higher)
+                    || (second_elf_lower >= first_elf_lower
+                        && second_elf_higher <= first_elf_higher);
+                let is_matching_borders =
+                    first_elf_lower == second_elf_higher || first_elf_higher == second_elf_lower;
+                let first_contains_second_lower =
+                    first_elf_higher > second_elf_lower && first_elf_lower < second_elf_lower;
+                let first_contains_second_higher =
+                    first_elf_higher > second_elf_higher && first_elf_lower < second_elf_higher;
+                let second_contains_first_lower =
+                    second_elf_higher > first_elf_lower && second_elf_lower < first_elf_lower;
+                let second_contains_first_higher =
+                    second_elf_higher > first_elf_higher && second_elf_lower < first_elf_higher;
+
+                if is_fully_contained
+                    || is_matching_borders
+                    || first_contains_second_lower
+                    || first_contains_second_higher
+                    || second_contains_first_lower
+                    || second_contains_first_higher
+                {
+                    Some(1)
+                } else {
+                    None
+                }
+            })
+            .count();
+        Ok(fully_contained as u64)
+    }
+}
+
+test_day!(
+    Day4,
+    2,
+    4,
+    r#"2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8"#
+);
--- rust-2022/src/main.rs
+++ rust-2022/src/main.rs
@@ -1,10 +1,11 @@
 use aoc_utils::{read_input, run_day, AdventError, Day};
 
-use crate::{day1::Day1, day2::Day2, day3::Day3};
+use crate::{day1::Day1, day2::Day2, day3::Day3, day4::Day4};
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() -> Result<(), AdventError> {
     println!("==== Advent of Code 2022 ====");
@@ -12,6 +13,7 @@ fn main() -> Result<(), AdventError> {
     run_day!(Day1, 1);
     run_day!(Day2, 2);
     run_day!(Day3, 3);
+    run_day!(Day4, 4);
 
     Ok(())
 }
