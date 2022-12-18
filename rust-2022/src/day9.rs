d53ffc591fc42e46993319c40ab953c62b513f62
Lane Sawyer <github@lanesawyer.dev>
2022-12-18 12:46:12 -0800

    Helper functions, still broken

--- rust-2022/src/day9.rs
+++ rust-2022/src/day9.rs
@@ -14,6 +14,25 @@ impl Position {
     fn new(x: i64, y: i64) -> Self {
         Position { x, y }
     }
+
+    fn trailing_position(self, direction: &str) -> Self {
+        let trailing_x = if self.x < 0 { 1 } else { -1 };
+        let trailing_y = if self.y < 0 { 1 } else { - 1 };
+        match direction {
+            "R" => Position::new(self.x + trailing_x, self.y),
+            "L" => Position::new(self.x - trailing_x, self.y),
+            "U" => Position::new(self.x, self.y + trailing_y),
+            "D" => Position::new(self.x, self.y - trailing_y),
+            _ => unreachable!("Invalid position"),
+        }
+    }
+
+    fn distance_from(self, other: &Position) -> (i64, i64) {
+        let x_distance = self.x - other.x;
+        let y_distance = self.y - other.y;
+
+        (x_distance, y_distance)
+    }
 }
 
 impl Day for Day9 {
@@ -41,44 +60,39 @@ impl Day for Day9 {
                     _ => unreachable!("Invalid position"),
                 };
 
-                let x_distance = head_position.x - tail_position.x;
-                let y_distance = head_position.y - tail_position.y;
+                // let x_distance = head_position.x - tail_position.x;
+                // let y_distance = head_position.y - tail_position.y;
 
-                // println!("x dist: {}, y dist: {}", x_distance, y_distance);
+                let (x_distance, y_distance) = head_position.distance_from(&tail_position);
+
+                println!("x dist: {}, y dist: {}", x_distance, y_distance);
 
                 // There's probably a smarter way to do it than all these cardinal directions
                 // Also I think when it goes into the negative these are getting messed up
-                // Directly right
-                if x_distance > 1 && y_distance == 0 {
-                    tail_position = Position::new(head_position.x - 1, tail_position.y);
-                }
-                // Directly left
-                else if x_distance < -1 && y_distance == 0 {
-                    tail_position = Position::new(head_position.x + 1, tail_position.y);
-                }
-                // Directly up
-                else if y_distance > 1 && x_distance == 0 {
-                    tail_position = Position::new(head_position.x, tail_position.y + 1);
-                }
-                // Directly down
-                else if y_distance < -1 && x_distance == 0 {
-                    tail_position = Position::new(head_position.x, tail_position.y - 1);
-                }
-                // Up-right, but only snap to it when the distance is two
-                else if x_distance > 0 && y_distance > 1 {
-                    tail_position = Position::new(head_position.x, head_position.y - 1);
-                }
-                // Up-left, but only snap to it when the distance is two
-                else if x_distance < -1 && y_distance > 0 {
-                    tail_position = Position::new(head_position.x + 1, head_position.y);
-                }
-                // Down-right, but only snap to it when the distance is two
-                else if x_distance > 1 && y_distance < 0 {
-                    tail_position = Position::new(head_position.x - 1, head_position.y);
-                }
-                // Down-left, but only snap to it when the distance is two
-                else if x_distance < -1 && y_distance < 0 {
-                    tail_position = Position::new(head_position.x + 1, head_position.y);
+                let should_move_tail =
+                    // Left or right
+                    (!(-1..=1).contains(&x_distance) && y_distance == 0)
+                    // Up or down
+                    || (!(-1..=1).contains(&y_distance) && x_distance == 0)
+                    // up right
+                    // || (x_distance > 0 && y_distance > 1)
+                    // up left
+                    // || (x_distance < 0 && y_distance > 1)
+                    // down right
+                    || (!(0..1).contains(&y_distance) && x_distance > 0)
+                    // down left
+                    // || (x_distance < 0 && y_distance < -1)
+                    // right up
+                    || (!(0..1).contains(&x_distance) && y_distance > 0);
+                    // left up
+                    // || (x_distance < -1 && y_distance > 0)
+                    // right down
+                    // || (x_distance > 1 && y_distance < 0)
+                    // left down
+                    // || (x_distance < -1 && y_distance > 0);
+
+                if should_move_tail {
+                    tail_position = head_position.trailing_position(direction);
                 }
 
                 tail_visited.insert(tail_position);
