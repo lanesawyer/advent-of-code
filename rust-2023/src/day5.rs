3e985bfd01d41164ac2b30394376b2f1c258098f
Lane Sawyer <github@lanesawyer.dev>
2023-12-04 23:39:01 -0800

    Day 5 Part 2! And Part 1 cleanups

--- rust-2023/src/day5.rs
+++ rust-2023/src/day5.rs
@@ -14,7 +14,6 @@ impl Day for Day5 {
         // Gotta skip the first mapping line, otherwise every map is off by one
         lines.next();
 
-        // Gross, why is it in a vec, there's got to be a better way. 8 seconds to run!!!??? 🤮
         let seed_to_soil: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
         let soil_to_fertilizer: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
         let fertilizer_to_water: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
@@ -26,100 +25,69 @@ impl Day for Day5 {
         let lowest_location_for_seed = seeds
             .map(|seed| {
                 let seed = seed.parse().unwrap();
-                let soil = seed_to_soil
-                    .iter()
-                    .find(|source| source.source_range.contains(&seed))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == seed)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(seed);
-                let fertilizer = soil_to_fertilizer
-                    .iter()
-                    .find(|source| source.source_range.contains(&soil))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == soil)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(soil);
-                let water = fertilizer_to_water
-                    .iter()
-                    .find(|source| source.source_range.contains(&fertilizer))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == fertilizer)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(fertilizer);
-                let light = water_to_light
-                    .iter()
-                    .find(|source| source.source_range.contains(&water))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == water)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(water);
-                let temperature = light_to_temperature
-                    .iter()
-                    .find(|source| source.source_range.contains(&light))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x: usize| x == light)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(light);
-                let humidity = temperature_to_humidity
-                    .iter()
-                    .find(|source| source.source_range.contains(&temperature))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == temperature)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(temperature);
-                let location = humidity_to_location
-                    .iter()
-                    .find(|source| source.source_range.contains(&humidity))
-                    .map(|found_item| {
-                        let position = found_item
-                            .source_range
-                            .clone()
-                            .position(|x| x == humidity)
-                            .unwrap();
-                        found_item.destination_range.start + position
-                    })
-                    .unwrap_or(humidity);
-
-                location
+                let soil = find_destination_mapping(&seed_to_soil, seed);
+                let fertilizer = find_destination_mapping(&soil_to_fertilizer, soil);
+                let water = find_destination_mapping(&fertilizer_to_water, fertilizer);
+                let light = find_destination_mapping(&water_to_light, water);
+                let temperature = find_destination_mapping(&light_to_temperature, light);
+                let humidity = find_destination_mapping(&temperature_to_humidity, temperature);
+                find_destination_mapping(&humidity_to_location, humidity)
             })
             .min();
 
         Ok(lowest_location_for_seed.unwrap().try_into().unwrap())
     }
 
-    fn part_2(_input: &str) -> Result<Answer, AdventError> {
-        Ok(2)
+    fn part_2(input: &str) -> Result<Answer, AdventError> {
+        let mut lines = input_to_trimmed_lines(input);
+
+        let seed_line = lines.next().unwrap();
+        let seeds_ranges_parsed = seed_line
+            .split(':')
+            .nth(1)
+            .unwrap()
+            .split_whitespace()
+            .collect::<Vec<&str>>(); // TODO: Why do I have to collect first before chunking?
+        let seed_ranges = seeds_ranges_parsed.chunks(2).map(|chunk| {
+            let start = chunk[0].parse::<usize>().unwrap();
+            let length = chunk[1].parse::<usize>().unwrap();
+            Range {
+                start,
+                end: start + length,
+            }
+        });
+
+        // Gotta skip the first mapping line, otherwise every map is off by one
+        lines.next();
+
+        let seed_to_soil: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let soil_to_fertilizer: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let fertilizer_to_water: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let water_to_light: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let light_to_temperature: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let temperature_to_humidity: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+        let humidity_to_location: Vec<_> = lines.by_ref().map_while(line_to_source).collect();
+
+        let lowest_location_for_seed = seed_ranges
+            .map(|seed_range| {
+                let min_in_range = seed_range
+                    .map(|seed| {
+                        let soil = find_destination_mapping(&seed_to_soil, seed);
+                        let fertilizer = find_destination_mapping(&soil_to_fertilizer, soil);
+                        let water = find_destination_mapping(&fertilizer_to_water, fertilizer);
+                        let light = find_destination_mapping(&water_to_light, water);
+                        let temperature = find_destination_mapping(&light_to_temperature, light);
+                        let humidity =
+                            find_destination_mapping(&temperature_to_humidity, temperature);
+                        find_destination_mapping(&humidity_to_location, humidity)
+                    })
+                    .min();
+
+                min_in_range.unwrap()
+            })
+            .min();
+
+        Ok(lowest_location_for_seed.unwrap().try_into().unwrap())
     }
 }
 
@@ -146,6 +114,20 @@ fn line_to_source(line: String) -> Option<Source> {
     })
 }
 
+fn find_destination_mapping(mapping: &[Source], source_value: usize) -> usize {
+    let source_with_value = mapping
+        .iter()
+        .find(|source| source.source_range.contains(&source_value));
+
+    match source_with_value {
+        Some(source) => {
+            let start = source.source_range.start;
+            source.destination_range.start + (source_value - start)
+        }
+        None => source_value,
+    }
+}
+
 #[derive(Debug)]
 struct Source {
     destination_range: Range<usize>,
@@ -155,7 +137,7 @@ struct Source {
 test_day!(
     Day5,
     35,
-    30,
+    46,
     r#"
     seeds: 79 14 55 13
 
