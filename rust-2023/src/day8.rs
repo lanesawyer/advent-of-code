887754315af11172e0dc1ba78ea894e68ef11d66
Lane Sawyer <github@lanesawyer.dev>
2023-12-07 22:44:06 -0800

    Part 2, cleanup, comments

--- rust-2023/src/day8.rs
+++ rust-2023/src/day8.rs
@@ -28,7 +28,8 @@ impl Day for Day8 {
         Ok(steps)
     }
 
-    // Fairly certain brute-forcing this would work, but it's going to take way too long.
+    // At first I brute forced it, but it would take too long. Accidentally spoiled myself that least common multiplier
+    // would work. My part two start should have a shameful asterisk next to it.
     fn part_2(input: &str) -> Result<Answer, AdventError> {
         let (instructions, network) = parse_instructions_and_network(input);
 
@@ -38,30 +39,34 @@ impl Day for Day8 {
             .cloned()
             .collect();
 
-        let mut steps = 0;
-        let mut instruction_position = 0;
-        let mut network_names = starting_network_names.clone();
+        let steps_where_all_sync_up: u64 = starting_network_names
+            .iter()
+            .map(|name| {
+                let mut steps = 0;
+                let mut instruction_position = 0;
+                let mut network_name = name.to_string();
 
-        // Walk the network until we find _all_ the next network names end in Z,
-        // looping to the beginning of the instructions if we reach the end
-        while !network_names.iter().all(|name| name.ends_with('Z')) {
-            if instruction_position >= instructions.len() {
-                instruction_position = 0;
-            }
+                // Walk the network until we find zzz, looping to the beginning of the instructions if we reach the end
+                while !network_name.ends_with('Z') {
+                    if instruction_position >= instructions.len() {
+                        instruction_position = 0;
+                    }
 
-            dbg!(steps);
-            let next_instruction = instructions[instruction_position];
+                    let next_instruction = instructions[instruction_position];
+                    network_name = get_next_network_name(&network, &network_name, next_instruction);
 
-            let next_network_names: Vec<String> = network_names.iter().map(|network_name| {
-                get_next_network_name(&network, network_name, next_instruction)
-            }).collect();
+                    instruction_position += 1;
+                    steps += 1;
+                }
 
-            network_names = next_network_names;
-            instruction_position += 1;
-            steps += 1;
-        }
+                steps
+            })
+            // Once we have the steps for each network, we need to find the least common multiplier
+            // which is the number of steps where they all sync up. Much faster than manually walking
+            // the network, which would take days, weeks, or months.
+            .fold(1, |acc, steps| least_common_multiplier(acc, steps as u64));
 
-        Ok(steps)
+        Ok(steps_where_all_sync_up)
     }
 }
 
@@ -100,6 +105,25 @@ fn get_next_network_name(
             panic!("Unknown instruction")
         }
     }
+}
+
+/// Recursive function to find greatest common denominator
+/// Basically, keep dividing the two numbers until the remainder is 0
+/// And you've found the one where they match up!
+fn greatest_common_denominator(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        greatest_common_denominator(b, a % b)
+    }
+}
+
+/// Function to fined the least common multiplier
+/// Following the formula:
+/// a * b / gcd(a, b)
+/// Where gcd is #[greatest_common_denominator]
+fn least_common_multiplier(a: u64, b: u64) -> u64 {
+    (a * b) / greatest_common_denominator(a, b)
 }
 
 test_day!(
