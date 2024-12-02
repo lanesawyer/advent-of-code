f714bf282c9d808e1a102c5f3b309ed96150dd97
Lane Sawyer <github@lanesawyer.dev>
2024-12-02 00:35:16 -0800

    2024 day 2 part 2 works!

--- rust-2024/src/day2.rs
+++ rust-2024/src/day2.rs
@@ -5,56 +5,85 @@ pub struct Day2;
 impl Day for Day2 {
     fn part_1(input: &str) -> Result<Answer, AdventError> {
         let total_safe = input_to_trimmed_lines(input)
-            .map(is_safe_checker)
+            .map(|line| is_safe_checker(line, None))
             .filter(|is_safe| *is_safe)
             .count();
         Ok(total_safe as u64)
     }
 
     fn part_2(input: &str) -> Result<Answer, AdventError> {
-        Ok(1)
+        let total_safe = input_to_trimmed_lines(input)
+            .map(|line| {
+                let line_passed_with_none_removed = is_safe_checker(line.clone(), None);
+                if line_passed_with_none_removed {
+                    return true;
+                }
+
+                // If it didn't fail on the first pass, go through all the possible indexes.
+                // If any of them pass once an item is removed, then it's safe!
+                (0..line.split_whitespace().count())
+                    .any(|index| is_safe_checker(line.clone(), Some(index)))
+            })
+            .filter(|is_safe| *is_safe)
+            .count();
+
+        Ok(total_safe as u64)
     }
 }
 
-fn is_safe_checker(line: String) -> bool {
+fn is_safe_checker(line: String, index_to_skip: Option<usize>) -> bool {
     let mut is_safe = true;
     let mut is_increasing = false;
     let mut is_decreasing = false;
     let mut prev_num: Option<u32> = None;
 
-    line.split_whitespace().for_each(|num| {
-        let num = num.parse::<u32>().unwrap();
+    line.split_whitespace()
+        .enumerate()
+        .for_each(|(index, num)| {
+            // We skip indexes for part two to check if failure ones would've passed if one item was removed
+            if index_to_skip.is_none() || index_to_skip.unwrap() != index {
+                let num = num.parse::<u32>().unwrap();
 
-        // If we're already not safe, just finish the loop so we can get on with life
-        if !is_safe {
-            return;
-        }
+                // If we're already not safe, just finish the loop so we can get on with life
+                if !is_safe {
+                    return;
+                }
 
-        if let Some(pn) = prev_num {
-            let diff = num.abs_diff(pn);
-            if !(1..=3).contains(&diff) {
-                println!("{} {} {}, was too big a gap", num, pn, diff);
-                is_safe = false;
+                // Only need to do some analysis if there is a previous number to check against
+                if let Some(pn) = prev_num {
+                    // If we're outside the safety range, we can immediately mark as not safe and stop looking
+                    let diff = num.abs_diff(pn);
+                    if !(1..=3).contains(&diff) {
+                        is_safe = false;
+                        return;
+                    }
+
+                    //  If the number is greater than the previous number and we've never seen a decrease,
+                    //  we mark it as increasing
+                    if num > pn && !is_decreasing {
+                        is_increasing = true;
+                    }
+                    // If the number is greater than the previous number and we've seen a decrease, that means
+                    // they all aren't increasing so it's marked as not safe and we can stop looking
+                    else if num > pn && is_decreasing {
+                        is_safe = false;
+                        return;
+                    }
+                    // Same logic as above, but for decreasing
+                    else if num < pn && !is_increasing {
+                        is_decreasing = true;
+                    }
+                    // Same logic above, but for decreasing not holding true
+                    else if num < pn && is_increasing {
+                        is_safe = false;
+                        return;
+                    }
+                }
+
+                prev_num = Some(num);
             }
+        });
 
-            // This logic is broken
-            if num > pn && !is_decreasing {
-                println!("{} > {} and we've never been decreasing", num, pn);
-                is_increasing = true;
-            } else if num > pn && is_decreasing {
-                is_safe = false;
-            } else if num < pn && !is_increasing {
-                println!("{} < {} and we've never been increasing", num, pn);
-                is_decreasing = true;
-            } else if num < pn && is_increasing {
-                is_safe = false;
-            }
-        }
-
-        prev_num = Some(num);
-    });
-
-    println!("{} -----", is_safe);
     is_safe
 }
 
