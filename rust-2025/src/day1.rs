e22ca9edffcfd84e405333122e755c8af31a196a
Lane Sawyer <github@lanesawyer.dev>
2025-12-06 16:59:34 -0800

    2025 Day 1 Part 2

--- rust-2025/src/day1.rs
+++ rust-2025/src/day1.rs
@@ -18,18 +18,11 @@ impl Day for Day1 {
                 // Make sure we wrap around the dial
                 let (turn, mut number) = instruction;
                 while number > 100 {
-                    println!("Wrapping around: {} > 100", turn);
                     number -= 100;
                 }
                 let new_position = match turn.as_str() {
                     "L" => {
                         let left_position = next_position - number;
-                        println!(
-                            "Next position after L{} from {}: {}",
-                            number,
-                            left_position + number,
-                            left_position
-                        );
                         if left_position >= 0 {
                             left_position
                         } else {
@@ -38,12 +31,6 @@ impl Day for Day1 {
                     }
                     "R" => {
                         let right_position = next_position + number;
-                        println!(
-                            "Next position after R{} from {}: {}",
-                            number,
-                            right_position - number,
-                            right_position
-                        );
                         if right_position < 100 {
                             right_position
                         } else {
@@ -52,8 +39,6 @@ impl Day for Day1 {
                     }
                     _ => 0,
                 };
-
-                println!("New position: {}", new_position);
 
                 if new_position == 0 {
                     (new_position, num_zeros + 1)
@@ -68,7 +53,60 @@ impl Day for Day1 {
     }
 
     fn part_2(input: &str) -> Result<Answer, AdventError> {
-        Ok(1)
+        let (_, num_zeros) = input_to_trimmed_lines(input)
+            .map(|line| {
+                let (turn, number) = line.split_at(1);
+                let number: i64 = number.parse().unwrap();
+
+                (turn.to_string(), number)
+            })
+            .fold((DIAL_START, 0), |acc, instruction| {
+                let (next_position, num_zeros) = acc;
+
+                let mut intermediate_zero_pass = 0;
+
+                // Make sure we wrap around the dial
+                let (turn, mut number) = instruction;
+                while number > 100 {
+                    intermediate_zero_pass += 1;
+                    number -= 100;
+                }
+                let new_position = match turn.as_str() {
+                    "L" => {
+                        let left_position = next_position - number;
+                        if left_position >= 0 {
+                            left_position
+                        } else {
+                            if next_position != 0 {
+                                intermediate_zero_pass += 1;
+                            }
+                            100 - left_position.abs()
+                        }
+                    }
+                    "R" => {
+                        let right_position = next_position + number;
+                        if right_position < 100 {
+                            right_position
+                        } else {
+                            if right_position != 100 {
+                                intermediate_zero_pass += 1;
+                            }
+                            right_position - 100
+                        }
+                    }
+                    _ => 0,
+                };
+
+                if new_position == 0 {
+                    (new_position, num_zeros + 1 + intermediate_zero_pass)
+                } else if new_position >= 0 && new_position < 100 {
+                    (new_position, num_zeros + intermediate_zero_pass)
+                } else {
+                    panic!("Invalid position: {}", new_position);
+                }
+            });
+
+        Ok(num_zeros as u64)
     }
 }
 
